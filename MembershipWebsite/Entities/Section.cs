5a3d851db338a2c92e575db198fc64496ded28c1
Luke Harris <luke.e.harris@gmail.com>
2017-10-27 13:31:20 -0500

    Created 'section' table

---
+++ MembershipWebsite/Entities/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MembershipWebsite.Entities
+{
+    [Table("Section")]
+    public class Section
+    {
+        // Makes it required and causes automatic incrementing when adding rows to the table
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+    }
+}
--- MembershipWebsite/Models/IdentityModels.cs
+++ MembershipWebsite/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MembershipWebsite.Entities;
 
 namespace MembershipWebsite.Models
 {
@@ -29,5 +30,7 @@ namespace MembershipWebsite.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Section> Sections { get; set; }
     }
 }
