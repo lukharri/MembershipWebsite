007db64242650a767a7f4c7c8cb729b0ad96ff7c
Cori <Cori@CORI>
2017-10-27 11:48:33 -0500

    Add project files.

---
+++ MembershipWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MembershipWebsite.MvcApplication" Language="C#" %>
---
+++ MembershipWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MembershipWebsite.Startup))]
+namespace MembershipWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
