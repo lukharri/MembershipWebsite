da8038479fcb81715636d34378b0c5ce2b3cfd26
Luke Harris <luke.e.harris@gmail.com>
2017-10-28 13:49:51 -0500

    Added extension methods for displaying items in drop-down menus in the views

---
+++ MembershipWebsite/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MembershipWebsite.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        /*
+         *  ToSelectListItem - convert a collection of type T into a collection of SelectLisItem 
+         *  Used to display drop-downs in MVC views
+         *  Params
+         *      - generic collection of ICollection<T> which contain the items to reflect over to get
+         *        their property values
+         *      - int for id of selected item
+         *  Returns a collection of type IEnumerable of SelectListItem
+         */
+
+        public static IEnumerable<SelectListItem>
+            ToSelectListItem<T>(
+            this ICollection<T> items, int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Title"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id")
+                       .Equals(selectedValue.ToString())
+                   };
+        }
+
+    }
+}
--- MembershipWebsite/Extensions/ReflectionExtensions.cs
+++ MembershipWebsite/Extensions/ReflectionExtensions.cs
@@ -7,5 +7,25 @@ namespace MembershipWebsite.Extensions
 {
     public static class ReflectionExtensions
     {
+        /*
+         *   Extension methods must be declared as public static and reside in a class of the same
+         *   Called from the toSelectList extension method
+         *      Fetches property values w/reflection from individual items in the collection
+         *      passed into the toSelectList item method
+         *   Params
+         *      generic type T represents the item to reflect over to read its property values
+         *      string for the name of the properties we want to fetch
+         *   Returns a string representing the property value
+         *   First param in an extension method has to be preceded by the 'this' keyword to make it
+         *      possible to call the method on any object w/a data type defined by this parameter
+         *   The generic type T makes it possible to use it on any type
+         */
+        public static string GetPropertyValue<T>(
+            this T item, string propertyName)
+        {
+            return item.GetType()
+                .GetProperty(propertyName)
+                .GetValue(item, null).ToString();
+        }
     }
 }
