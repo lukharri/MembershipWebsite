bbb463e3d93c25d1ce356559a54658cff1e36808
Luke Harris <luke.e.harris@gmail.com>
2017-11-06 14:58:52 -0600

    Implemented thumbnail comparer for testing equality by product id

---
+++ MembershipWebsite/Comparers/ThumbnailEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using MembershipWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MembershipWebsite.Comparers
+{
+    public class ThumbnailEqualityComparer : IEqualityComparer<ThumbnailModel>
+    {
+        public bool Equals(ThumbnailModel thumb1, ThumbnailModel thumb2)
+        {
+            return thumb1.ProductId.Equals(thumb2.ProductId);
+        }
+
+        public int GetHashCode(ThumbnailModel thumb)
+        {
+            return thumb.ProductId;
+        }
+    }
+}
