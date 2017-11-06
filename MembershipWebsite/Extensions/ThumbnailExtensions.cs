35432b56006c4d277db61249004c4150e6cc3db8
Luke Harris <luke.e.harris@gmail.com>
2017-11-06 15:23:50 -0600

    Added getThumbnails extension method for fetching thumbnail data and storing in the model

--- MembershipWebsite/Extensions/ThumbnailExtensions.cs
+++ MembershipWebsite/Extensions/ThumbnailExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MembershipWebsite.Models;
+﻿using MembershipWebsite.Comparers;
+using MembershipWebsite.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -27,6 +28,38 @@ namespace MembershipWebsite.Extensions
             catch { }
 
             return new List<int>();
+        }
+
+
+        public static async Task<IEnumerable<ThumbnailModel>> GetThumbnailsAsync (
+            this List<ThumbnailModel> thumbnails, string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<ThumbnailModel>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                var subscriptionIds = await GetSubscriptionIdsAsync(userId, db);
+                thumbnails = await (
+                    from ps in db.SubscriptionProducts
+                    join p in db.Products on ps.ProductId equals p.Id
+                    join plt in db.ProductLinkTexts on p.ProductLinkTextId equals plt.Id
+                    join pt in db.ProductTypes on p.ProductTypeId equals pt.Id
+                    where subscriptionIds.Contains(ps.SubscriptionId)
+                    select new ThumbnailModel
+                    {
+                        ProductId = p.Id,
+                        SubscriptionId = ps.SubscriptionId,
+                        Title = p.Title,
+                        Description = p.Description,
+                        ImageUrl = p.ImageUrl,
+                        Link = "/ProductContent/Index/" + p.Id,
+                        TagText = plt.Title,
+                        Content = pt.Title
+                    }).ToListAsync();
+            }
+            catch { }
+            return thumbnails.Distinct(new ThumbnailEqualityComparer()).OrderBy(o => o.Title);
         }
     }
 }
