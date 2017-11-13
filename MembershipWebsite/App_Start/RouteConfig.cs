f4e1a238d45d6292d5e87d9c135af8d5cc4ada5c
Luke Harris <luke.e.harris@gmail.com>
2017-11-13 17:59:30 -0600

    Created custom route for displaying content from ProductContent controller

--- MembershipWebsite/App_Start/RouteConfig.cs
+++ MembershipWebsite/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace MembershipWebsite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ProductContent",
+                url: "{controller}/{action}/{productId}/{itemId}",
+                defaults: new { controller = "ProductContent", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
