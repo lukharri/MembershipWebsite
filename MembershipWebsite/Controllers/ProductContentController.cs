b04737fa488fc44967214a47f68352cc6500f59c
Luke Harris <luke.e.harris@gmail.com>
2017-11-13 13:07:51 -0600

    Added controller and model for displaying a product's contents

---
+++ MembershipWebsite/Controllers/ProductContentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MembershipWebsite.Controllers
+{
+    // Reachable only by users who have logged in
+    [Authorize]
+    public class ProductContentController : Controller
+    {
+        // GET: ProductContent
+        public async Task<ActionResult> Index(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ MembershipWebsite/Models/ProductItemRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MembershipWebsite.Models
+{
+    // Handles displaying 1 productItem in a section
+    public class ProductItemRow
+    {
+        public int ItemId { get; set; }
+        public string ImagaUrl { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDownload { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
