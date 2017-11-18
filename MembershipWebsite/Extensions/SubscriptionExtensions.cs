a6d2cc14fd7f0f97afd60825ce26f54e69bfae28
Luke Harris <luke.e.harris@gmail.com>
2017-11-18 14:53:12 -0600

    Created method to fetch a subscription from a user-entered registration code.

---
+++ MembershipWebsite/Extensions/SubscriptionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using MembershipWebsite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MembershipWebsite.Extensions
+{
+    public static class SubscriptionExtensions
+    {
+        public static async Task<int> GetSubscriptionIdByRegistrationCode(
+            this IDbSet<Subscription> subscription, string code)
+        {
+            try
+            {
+                if (subscription == null || code == null || code.Equals(string.Empty))
+                    return Int32.MinValue;
+
+                var subscriptionId = await (
+                    from s in subscription
+                    where s.RegistrationCode.Equals(code)
+                    select s.Id).FirstOrDefaultAsync();
+
+                return subscriptionId;
+            }
+            catch { return Int32.MinValue; }
+    }
+}
