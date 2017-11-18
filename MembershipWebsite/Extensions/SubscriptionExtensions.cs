c528e2345ca7da3f09a21ddc437869bd122447a0
Luke Harris <luke.e.harris@gmail.com>
2017-11-18 15:09:58 -0600

    Created method to register a subscription id to a user.

--- MembershipWebsite/Extensions/SubscriptionExtensions.cs
+++ MembershipWebsite/Extensions/SubscriptionExtensions.cs
@@ -26,5 +26,36 @@ namespace MembershipWebsite.Extensions
                 return subscriptionId;
             }
             catch { return Int32.MinValue; }
+        }
+
+
+        // Registers a subscription id matching the code entered to the logged in user
+        // by adding a new entry in the UserSubscription table
+        public static async Task Register(
+            this IDbSet<UserSubscription> userSubscription, int subscriptionId, string userId)
+        {
+            try
+            {
+                if (userSubscription == null ||
+                    subscriptionId.Equals(Int32.MinValue) ||
+                    userId.Equals(string.Empty))
+                    return;
+
+                var exists = await Task.Run(() => userSubscription.CountAsync(
+                    s => s.SubscriptionId.Equals(subscriptionId) &&
+                    s.UserId.Equals(userId))) > 0;
+
+                if (!exists)
+                    await Task.Run(() => userSubscription.Add(
+                        new UserSubscription
+                        {
+                            UserId = userId,
+                            SubscriptionId = subscriptionId,
+                            StartDate = DateTime.Now,
+                            EndDate = DateTime.MaxValue
+                        }));
+            }
+            catch { }
+        }
     }
 }
